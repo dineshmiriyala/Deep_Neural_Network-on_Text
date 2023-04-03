1935aa8d157e8a9690e1f58f5c06e1485766ecf4
dineshmiriyala <dineshmiriyala9968@gmail.com>
2023-04-02 22:34:36 -0400

    Testing

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from model import linear, tanh, BatchNorm1d, gen
+from model import linear, tanh, BatchNorm1d, gen, sequential, embedding, flatten
 import sys
 import os
 import torch
 import pickle
 import warnings
 import torch.nn.functional as F
-from tqdm import tqdm
+import matplotlib.pyplot as plt
+
 warnings.filterwarnings('ignore')
 
 torch.cuda.set_device(0)
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
-if not os.path.exists('data/trainingData.pkl') or not os.path.exists('data/encode_decode.pkl'):
+if not os.path.exists('data/trainingData_40.pkl') or not os.path.exists('data/encode_decode.pkl'):
     sys.exit("Training data file does not exist. Try running the pre_process.py\n")
 
-with open('data/trainingData.pkl', 'rb') as file:
+with open('data/trainingData_40.pkl', 'rb') as file:
     data = pickle.load(file)
 
 with open('data/encode_decode.pkl', 'rb') as file:
@@ -24,65 +25,53 @@ with open('data/encode_decode.pkl', 'rb') as file:
 
 X_train = data['X_train']
 y_train = data['y_train']
+X_val = data['X_val']
+y_val = data['y_val']
+X_test = data['Xtest']
+y_test = data['ytest']
 
 """x*w"""
 
-# I am squashing all the input probabilities into 60 dimensions, that is 60 features.
-n_emb = 120
+# I am squashing all the input probabilities into 240 dimensions, that is 240` features.
+n_emb = 240
 n_hidden = 300
-block_size = 20
+block_size = 40
 vocab_size = len(encode)
-probabilities = torch.randn((vocab_size, n_emb), generator = gen).cuda()
 
-layers = [linear(n_emb * block_size,n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, n_hidden, Bias = False), BatchNorm1d(n_hidden), tanh(),
-          linear(        n_hidden, vocab_size, Bias = False), BatchNorm1d(vocab_size)]
+model = sequential([
+    embedding(vocab_size, n_emb), flatten(), linear(n_emb * block_size, n_hidden, Bias=False), BatchNorm1d(n_hidden), tanh(),
+    linear(n_hidden, n_hidden, Bias=False), BatchNorm1d(n_hidden), tanh(),
+    linear(n_hidden, n_hidden, Bias=False), BatchNorm1d(n_hidden), tanh(),
+    linear(n_hidden, n_hidden, Bias=False), BatchNorm1d(n_hidden), tanh(),
+    linear(n_hidden, vocab_size)])
 
 with torch.no_grad():
     # making last layer less confident
-    layers[-1].gamma *= 0.1
+    model.layers[-1].weight *= 0.1
     # applying gain for all the remaining layers, kaiming init, 5/3
-    for layer in layers:
+    for layer in model.layers:
         if isinstance(layer, linear):
-            layer.weight *= 1.0  #5/3
+            layer.weight *= 1.0  # 5/3
 
-params = [probabilities] +[p for layer in layers for p in layer.parameters()]
-print('number of parameters: ',sum(p.nelement() for p in params)) # number of parameters
-for p in params:
+for p in model.parameters():
     p.requires_grad = True
 
-#training part
-steps = 100000
+# training part
+steps = 10000
 batch_size = 32
 lossi = []
 ud = []
 
 for i in range(steps):
     # mini batches
-    index = torch.randint(0, X_train.shape[0], (batch_size,), generator = gen)
+    index = torch.randint(0, X_train.shape[0], (batch_size,), generator=gen)
 
-    # forward pass
-    emb = probabilities[X_train[index]]
-    x = emb.view(emb.shape[0], -1) # concatenating the vectors
-    for layer in layers:
-        x = layer(x)
+    xindex, yindex = X_train[index], y_train[index]
+    # model
+    x = model(xindex)
     loss = F.cross_entropy(x, y_train[index])
-
     # backward pass
-    for layer in layers:
-        layer.out.retain_grad()
-    for p in params:
+    for p in model.parameters():
         p.grad = None
     loss.backward()
 
@@ -94,37 +83,45 @@ for i in range(steps):
         lr = 0.01
     else:
         lr = 0.001
-    for p in params:
+    for p in model.parameters():
         p.data += -lr * p.grad
 
     # tracking stats
-    if i % 10000 ==0:
+    if i % 10000 == 0:
         print(f'{i:7d}/{steps:7d}: {loss.item():.4f}')
     lossi.append(loss.log10().item())
     with torch.no_grad():
-        ud.append([((lr * p.grad).std() / p.data.std()).log10().item() for p in params])
+        ud.append([((lr * p.grad).std() / p.data.std()).log10().item() for p in model.parameters()])
 
-for layer in layers:
+print(f'Final loss: {loss.item()}')
+# plotting the loss function
+plt.plot(torch.tensor(lossi , device = 'cpu').view(-1, 1000).mean(-1))
+plt.title(f'Training loss')
+plt.ylabel("loss")
+plt.xlabel("iteration")
+plt.show()
+# turning off training mode
+for layer in model.layers:
     layer.training = False
 
-for _ in range(20):
 
-    out = []
-    context = [0] * block_size  # initialize with all ...
-    while True:
-        # forward pass the neural net
-        emb = probabilities[torch.tensor([context])]  # (1,block_size,n_embd)
-        x = emb.view(emb.shape[0], -1)  # concatenate the vectors
-        for layer in layers:
-            x = layer(x)
-        probs = F.softmax(x, dim=1)
-        # sample from the distribution
-        ix = torch.multinomial(probs, num_samples=1, generator=gen).item()
-        # shift the context window and track the samples
-        context = context[1:] + [ix]
-        out.append(ix)
-        # if we sample the special '.' token, break
-        if ix == 0:
-            break
+# evaluating validation loss
+@torch.no_grad()
+def split_loss(split):
+    loss_test = []
+    x, y = {'train': (X_train, y_train),
+            'val': (X_val, y_val),
+            'test': (X_test, y_test)}[split]
+    for i in range(x.shape[0] // 50):
+        index = torch.randint(0, x.shape[0], (100,))
+        logits = model(x[index])
+        lossi.append(F.cross_entropy(logits, y[index]))
+    plt.plot(torch.tensor(loss_test, device= 'cpu'))
+    plt.title(f'{split} loss')
+    plt.ylabel("loss")
+    plt.xlabel("iteration")
+    plt.show()
 
-    print(' '.join(decode[i] for i in out))  # decode and print the generated word
+split_loss('val')
+
+#%%
